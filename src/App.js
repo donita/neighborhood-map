5b786374a65676f29a1f8a2b3c2e10cdd0c4190f
Donita <mathisdonita@gmail.com>
2018-10-25 21:29:16 -0400

    feat: add Google Map Api function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  
+  function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+  
+  
+  
+  
   render() {
     return (
+      <main>
     <div id="map"></div>
+    </main>
     )
   }
 }
